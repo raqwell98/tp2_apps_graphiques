fd740303a0d52b729df84cd3ebf7a1518456da53
micpome <micpome@dinfo.cegepthetford.ca>
2017-10-16 18:09:41 -0400

    RIIIIIP

---
+++ Services/ValidateurId.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using Affaire;
+
+namespace Services
+{
+    class ValidateurId : ValidationRule
+    {
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            CollectionClientsObservable clients = new ScribeXML().recupererClients();
+            if (clients.ToList().First(x => x.Id == ((string)value)) == null)
+                return ValidationResult.ValidResult;
+            return new ValidationResult(false, "L'id de client éxiste déjà");
+        }
+    }
+}
